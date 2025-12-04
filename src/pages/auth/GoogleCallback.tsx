4be1c8cb5fe1726aaa82809cb8e2e767f3fcace1
mynolog <minholee.log@gmail.com>
2025-12-04 21:11:12 +0900

    fix: OAuth 콜백 처리 로직 수정

--- src/pages/auth/GoogleCallback.tsx
+++ src/pages/auth/GoogleCallback.tsx
@@ -1,6 +1,28 @@
 import { Spinner } from '@/components/ui/spinner'
+import { supabase } from '@/lib/supabaseClient'
+import { ROUTES } from '@/routes'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 export default function GoogleCallbackPage() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const handleOAuth = async () => {
+      const hash = window.location.hash.substring(1)
+      const params = new URLSearchParams(hash)
+      const access_token = params.get('access_token')
+      const refresh_token = params.get('refresh_token')
+
+      if (access_token && refresh_token) {
+        await supabase.auth.setSession({ access_token, refresh_token })
+        navigate(ROUTES.HOME)
+      }
+    }
+
+    handleOAuth()
+  }, [navigate])
+
   return (
     <div className="flex h-screen w-full items-center justify-center gap-4">
       <Spinner className="h-8 w-8 animate-spin text-orange-300" />
